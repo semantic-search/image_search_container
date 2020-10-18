c7cd38f77eb1b7c0334acd39de70a650d4c1ac19
jainal09 <jainal09gosalia@gmail.com>
2020-10-19 04:54:53 +0530

    gpu usage reduction applied

--- feature_extractor.py
+++ feature_extractor.py
@@ -3,13 +3,23 @@ from tensorflow.keras.applications import Xception
 from tensorflow.keras.applications.xception import preprocess_input
 import numpy as np
 from PIL import Image
+import tensorflow as tf
+from tensorflow.python.keras.backend import set_session
+
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.compat.v1.Session(config=config)
+graph = tf.compat.v1.get_default_graph()
+set_session(sess)
 
 # See https://keras.io/api/applications/ for details
 
 
 class FeatureExtractor:
     def __init__(self):
-        self.model = Xception(
+        with graph.as_default():
+            self.model = Xception(
                         weights="imagenet",
                         classes=1000,
                         classifier_activation="softmax",
@@ -20,7 +30,6 @@ class FeatureExtractor:
         Extract a deep feature from an input image
         Args:
             img: from PIL.Image.open(path) or tensorflow.keras.preprocessing.image.load_img(path)
-
         Returns:
             feature (np.ndarray): deep feature with the shape=(4096, )
         """
@@ -30,6 +39,7 @@ class FeatureExtractor:
         x = image.img_to_array(img)  # To np.array. Height x Width x Channel. dtype=float32
         x = np.expand_dims(x, axis=0)  # (H, W, C)->(1, H, W, C), where the first elem is the number of img
         x = preprocess_input(x)  # Subtracting avg values for each pixel
-        feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
+        with graph.as_default():
+            set_session(sess)
+            feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
         return feature / np.linalg.norm(feature)  # Normalize
-
